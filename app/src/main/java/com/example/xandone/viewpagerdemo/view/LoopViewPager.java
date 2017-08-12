fb1a381e7b09b7dc327858bd4cbcf7003b8c9f0a
xandone <765478955@qq.com>
2017-08-12 23:32:00 +0800

    loop

---
+++ app/src/main/java/com/example/xandone/viewpagerdemo/view/LoopViewPager.java
@@ -0,0 +1,20 @@
+package com.example.xandone.viewpagerdemo.view;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * author: xandone
+ * created on: 2017/8/12 23:31
+ */
+
+public class LoopViewPager extends ViewPager {
+    public LoopViewPager(Context context) {
+        this(context, null);
+    }
+
+    public LoopViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
